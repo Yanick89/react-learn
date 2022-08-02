23a1ec88bad171a521a304ad3ba3b6aeacaa14b2
Yanick <moussoundayannick89@gmail.com>
2022-08-02 05:35:51 +0100

    💥 NEW: try condition

--- src/jour-2/like.js
+++ src/jour-2/like.js
@@ -1,16 +1,20 @@
 import { useState } from "react"
 
 function Like (){
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
     const handlerCounter = () =>{
         setCounter(counter + 1)
     }
-
     return (
         <div className="btn-aime">
             <h2>Laisse un coeur si t'as aimé </h2>
             <button onClick={handlerCounter}>❤️</button>
             <span>{counter}</span>
+            { counter > 9 &&
+                <span>
+                    k
+                </span>
+            }
         </div>
     )
 }
